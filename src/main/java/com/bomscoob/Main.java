561c3344967b8a006c360503862a55375e247548
BomScoob12 <sorawitkraukham@gmail.com>
2024-10-31 23:25:26 +0700

    feat: basic CLI calculator

--- src/main/java/com/bomscoob/Main.java
+++ src/main/java/com/bomscoob/Main.java
@@ -1,13 +1,50 @@
 package com.bomscoob;
 
+import com.bomscoob.utils.impl.CalculatorOperations;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        CalculatorOperations operations = new CalculatorOperations();
         System.out.println("How many number do you need to calculator? ");
         int n = scanner.nextInt();
-        System.out.println(n);
+        int sum = 0;
+        int count = 2;
 
+        System.out.print("first number is : ");
+        int firstNum = scanner.nextInt();
+        sum += firstNum;
+
+        while (count <= n) {
+            System.out.printf("%d number is : ", count++);
+            int current = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.printf("Which operation for %d?", current);
+            System.out.print("[ +, -, *, / ] : ");
+            String op = scanner.nextLine();
+
+            switch (op) {
+                case "+" -> {
+                    sum = operations.plus(sum, current);
+                }
+                case "-" -> {
+                    sum = operations.subtract(sum, current);
+                }
+                case "*" -> {
+                    sum = operations.times(sum, current);
+                }
+                case "/" -> {
+                    sum = operations.divider(sum, current);
+                }
+                default -> {
+                    throw new IllegalArgumentException("No operation match.");
+                }
+            }
+
+            System.out.printf("sum is %d\n", sum);
+        }
     }
 }
